aa1d7c52a00ca71c9aa5c971a09e775fd0571367
Kranberry <60517786+Kranberry@users.noreply.github.com>
2020-12-11 16:03:55 +0100

    Reverse.cs

--- C#/Reverse.cs
+++ C#/Reverse.cs
@@ -4,6 +4,19 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        int cases = Int32.Parse(Console.ReadLine());
+        int[] reverse = new int[cases];
+
+        for(int i = 0; i < cases; i++)
+        {
+            reverse[i] = Int32.Parse(Console.ReadLine());
+        }
+
+        Array.Reverse(reverse);
+
+        for(int i = 0; i < cases; i++)
+        {
+            Console.WriteLine(reverse[i]);
+        }
     }
 }
